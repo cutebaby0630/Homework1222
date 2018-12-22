9ce9181ae40d4951a63ce072e79c102195fccbbc
cutebaby0630 <lovemath0630@gmail.com>
2018-12-22 16:13:45 +0800

    Bobble

---
+++ src/Booble.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Booble {
+	public static void main(String[] args) {
+		
+//			int n[] = { 51, 23, 6, 65, 15 ,89,100,1235,358};
+			Random r = new Random();
+			int n[] = new int[10];
+			for (int i = 0; i < 10; i++) {
+				n[i] = (int) r.nextInt((50) + 1);
+
+			}
+			System.out.print("排列前:{ ");
+			for(int num:n){
+				System.out.print(num + " ");
+			}
+			System.out.println("}");
+			for (int i = 0; i < n.length - 1; i++) {
+				for (int j = (i + 1); j < n.length; j++) {
+					if (n[i] > n[j]) {
+						int tmp = n[i];
+						n[i] = n[j];
+						n[j] = tmp;
+					}
+				}
+			}
+			System.out.print("排列後:{ ");
+			for(int num:n){
+				System.out.print(num + " ");
+			}
+			System.out.println("}");
+		}
+}
